f864b2cb29678d9c6ffe19b47d29eb695e203220
zeel0802 <zeel2252@gmail.com>
2024-11-28 17:50:38 -0600

    Add Calculator with methods and tests

--- src/main/java/com/csc/Calculator.java
+++ src/main/java/com/csc/Calculator.java
@@ -1,7 +1,74 @@
 package com.csc;
 
+import java.util.Arrays;
+
 public class Calculator {
-  public static void main(String[] args) {
-    System.out.println("This class can house your driver program and / or your functions.");
-  }
+
+    // Functions using For Loop
+    public int maximumUsingForLoop(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    public int minimumUsingForLoop(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num < min) {
+                min = num;
+            }
+        }
+        return min;
+    }
+
+    public int sumUsingForLoop(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    public double averageUsingForLoop(int[] nums) {
+        if (nums.length == 0) return 0;
+        return (double) sumUsingForLoop(nums) / nums.length;
+    }
+
+    // Functions using Java Stream API
+    public int maximumUsingStream(int[] nums) {
+        return Arrays.stream(nums).max().orElse(Integer.MIN_VALUE);
+    }
+
+    public int minimumUsingStream(int[] nums) {
+        return Arrays.stream(nums).min().orElse(Integer.MAX_VALUE);
+    }
+
+    public int sumUsingStream(int[] nums) {
+        return Arrays.stream(nums).sum();
+    }
+
+    public double averageUsingStream(int[] nums) {
+        return Arrays.stream(nums).average().orElse(0.0);
+    }
+
+    // Portfolio Add-Ons
+    public int[] evensOnly(int[] nums) {
+        return Arrays.stream(nums).filter(num -> num % 2 == 0).toArray();
+    }
+
+    public int[] oddsOnly(int[] nums) {
+        return Arrays.stream(nums).filter(num -> num % 2 != 0).toArray();
+    }
+
+    public int[] addFive(int[] nums) {
+        return Arrays.stream(nums).map(num -> num + 5).toArray();
+    }
+
+    public int[] squareNumbers(int[] nums) {
+        return Arrays.stream(nums).map(num -> num * num).toArray();
+    }
 }
--- src/test/java/com/csc/TestCalculator.java
+++ src/test/java/com/csc/TestCalculator.java
@@ -1,21 +1,62 @@
 package com.csc;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestCalculator {
 
-  Calculator calculator;
+    private final Calculator calculator = new Calculator();
 
-  @BeforeEach
-  void setUp() {
-    calculator = new Calculator();
-  }
+    @Test
+    void testMaximumFunctions() {
+        int[] nums = {1, 2, 3, 4, 5};
+        assertEquals(5, calculator.maximumUsingForLoop(nums));
+        assertEquals(5, calculator.maximumUsingStream(nums));
+    }
 
-  @Test
-  void itWorks() {
-    assertEquals(true, true);
-  }
+    @Test
+    void testMinimumFunctions() {
+        int[] nums = {1, 2, 3, 4, 5};
+        assertEquals(1, calculator.minimumUsingForLoop(nums));
+        assertEquals(1, calculator.minimumUsingStream(nums));
+    }
+
+    @Test
+    void testSumFunctions() {
+        int[] nums = {1, 2, 3, 4, 5};
+        assertEquals(15, calculator.sumUsingForLoop(nums));
+        assertEquals(15, calculator.sumUsingStream(nums));
+    }
+
+    @Test
+    void testAverageFunctions() {
+        int[] nums = {1, 2, 3, 4, 5};
+        assertEquals(3.0, calculator.averageUsingForLoop(nums));
+        assertEquals(3.0, calculator.averageUsingStream(nums));
+    }
+
+    @Test
+    void testEvensOnly() {
+        int[] nums = {1, 2, 3, 4, 5};
+        assertArrayEquals(new int[]{2, 4}, calculator.evensOnly(nums));
+    }
+
+    @Test
+    void testOddsOnly() {
+        int[] nums = {1, 2, 3, 4, 5};
+        assertArrayEquals(new int[]{1, 3, 5}, calculator.oddsOnly(nums));
+    }
+
+    @Test
+    void testAddFive() {
+        int[] nums = {1, 2, 3, 4, 5};
+        assertArrayEquals(new int[]{6, 7, 8, 9, 10}, calculator.addFive(nums));
+    }
+
+    @Test
+    void testSquareNumbers() {
+        int[] nums = {1, 2, 3, 4, 5};
+        assertArrayEquals(new int[]{1, 4, 9, 16, 25}, calculator.squareNumbers(nums));
+    }
 }
